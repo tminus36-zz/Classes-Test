8040a55a5a379d5da30cfa3af3f904f632c8ba43
Tim Harris <tph@securednetworks.us>
2014-05-16 02:40:42 -0500

    Testing local Secondary-Branch -- variables added

--- Classes.cpp
+++ Classes.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() 
 {
 
+int SecondBranch = 1;
+int ThirdBranch = SecondBranch;
+
 	cout << "Hi there!\n";
+	cout << SecondBranch << "\n is also " << ThirdBranch << endl;
+	
 	return 0;
 }
