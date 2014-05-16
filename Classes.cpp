394f6e22dddaa4c8f09e9383e804f1a8f39d01b2
Tim Harris <tph@securednetworks.us>
2014-05-16 02:58:44 -0500

    Cleanup after Merge

--- Classes.cpp
+++ Classes.cpp
@@ -4,12 +4,10 @@ using namespace std;
 int main() 
 {
 
-<<<<<<< HEAD
 int SecondBranch = 1;
 int ThirdBranch = SecondBranch;
-=======
+
 int Uses = 0;
->>>>>>> master
 
 	cout << "Hi there!\n";
 	cout << SecondBranch << "\n is also " << ThirdBranch << endl;
